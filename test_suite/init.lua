1e2d6774dbca7545a5641b77f1e3e7f21e12bdb0
Alexander Kabanov <alexander.kabanov@gmail.com>
2010-10-14 13:17:32 +0100

    Added a convenience random sample function to the Lua test init script

--- test_suite/init.lua
+++ test_suite/init.lua
@@ -3,3 +3,9 @@
 require("rng_lua_swig_wrap")
 rng = rng_lua_swig_wrap
 g = rng.mtwistGenerator()
+
+sample = function(g,N)
+   t = {}
+   for i=1,N do t[#t+1] = g() end
+   return table.concat(t,' ')
+end
